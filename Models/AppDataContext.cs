8bff11e3f4b41a687f1eb9d0f0d3220c81a1af84
AryanGargeya-06 <aryan06.uk@gmail.com>
2026-03-11 09:25:06 +0000

    Add files via upload

---
+++ Models/AppDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BESAFE.Models
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options): 
+            base(options)
+                { }
+            public DbSet<product>product { get; set; }  
+    }
+}
